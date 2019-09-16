c787e087c858a4ba6316377c887a11c60dfab27b
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-09-16 21:08:02 +0200

    Add InstrumentListViewController to storyboard

---
+++ Instruments/Controller/InstrumentListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InstrumentListViewController.swift
+//  Instruments
+//
+//  Created by Andreas Lüdemann on 16/09/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentListViewController: UITableViewController {
+    
+}
